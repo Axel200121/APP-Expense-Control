0c116c8ba3743bc23302de676c3f7efcb1d5b22d
Axel200121 <axelmartinez212001@gmail.com>
2024-03-17 11:01:45 -0600

    Creación de la api

---
+++ api_control_gastos/src/main/java/com/ams/developer/api/expense/control/ApiControlGastosApplication.java
@@ -0,0 +1,13 @@
+package com.ams.developer.api.expense.control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiControlGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiControlGastosApplication.class, args);
+	}
+
+}
---
+++ api_control_gastos/src/test/java/com/ams/developer/api/expense/control/ApiControlGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ams.developer.api.expense.control;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiControlGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
