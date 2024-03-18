7add0b3fbc939d45c068d07baf8dbcc6d0392381
Axel200121 <axelmartinez212001@gmail.com>
2024-03-17 20:54:24 -0600

    agregando metodos repository de usuario  para usar en la autetificación

--- api_control_gastos/src/main/java/com/ams/developer/api/expense/control/repository/UsuarioRepository.java
+++ api_control_gastos/src/main/java/com/ams/developer/api/expense/control/repository/UsuarioRepository.java
@@ -1,7 +1,12 @@
 package com.ams.developer.api.expense.control.repository;
 
+import com.ams.developer.api.expense.control.model.EstadosModel;
 import com.ams.developer.api.expense.control.model.UsuariosModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<UsuariosModel,String> {
+    UsuariosModel findByCorreo(String correo);
+    Optional<UsuariosModel> findByCorreoAndEstadosId(String correo, EstadosModel estado);
 }
