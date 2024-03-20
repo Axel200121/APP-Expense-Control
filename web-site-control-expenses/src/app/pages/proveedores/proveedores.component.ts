9a2d544414ca20abe0ac7f6468f303ae75aedc2d
Axel200121 <axelmartinez212001@gmail.com>
2024-03-20 12:35:59 -0600

    Agregnado metodo guardar proveedor

--- web-site-control-expenses/src/app/pages/proveedores/proveedores.component.ts
+++ web-site-control-expenses/src/app/pages/proveedores/proveedores.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
 import { HeaderComponent } from '../../components/header/header.component';
 import { SidebarComponent } from '../../components/sidebar/sidebar.component';
@@ -7,11 +7,14 @@ import { RouterLink } from '@angular/router';
 import { ProveedoresService } from '../../services/proveedores.service';
 import { ProveedoresDto } from '../../interface/proveedores-dto';
 import { ApiResponseDto } from '../../interface/api-response-dto';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-proveedores',
   standalone: true,
-  imports: [HeaderComponent,SidebarComponent,FooterComponent,RouterLink],
+  imports: [FormsModule, HeaderComponent,SidebarComponent,FooterComponent,RouterLink],
   templateUrl: './proveedores.component.html',
   styleUrl: './proveedores.component.css'
 })
@@ -20,9 +23,20 @@ export class ProveedoresComponent implements OnInit {
   public providersList!:ProveedoresDto[];
   public apiResponseDto!:ApiResponseDto;
 
+  //variables para modal guardar
+  @ViewChild("myModalConfig",{static:false})
+  myModalConfig!:TemplateRef<any>
+  public modalTitle!:string;
+
+  //inicializar dto proveedor para guardar
+  public providerDto:ProveedoresDto={
+    nombre:''
+  }
+
   constructor(
     private _authService:AuthService,
-    private _proveedoresService:ProveedoresService
+    private _proveedoresService:ProveedoresService,
+    private _modalService:NgbModal
     ){}
 
 
@@ -40,6 +54,44 @@ export class ProveedoresComponent implements OnInit {
         console.log('Error: '+error.message)
       }
     })
+  }
+
+
+  public switchForm(){
+    if(this.modalTitle=="Agrega"){
+      this.saveProvider()
+    }else if (this.modalTitle=="Modifica") {
+      
+    }
+  }
+
+  openSaveModalProvider(){
+    this._modalService.open(this.myModalConfig,{size:'lg'});
+    this.modalTitle="Agrega"
+  }
+
+  private saveProvider(){
+    this._proveedoresService.setProvider(this.providerDto,this._authService.getToken()).subscribe({
+      next: response =>{
+        swal.fire({
+          icon:'success',
+          title:'OK',
+          text:"Proveedor agregado correctamente"
+        })
+        setInterval(()=>{
+          window.location.href ="/providers"
+          this.getAllProviders()
+        },1500)
+        this.getAllProviders()
+      },error: error =>{
+        console.log("Error--> "+ error.message)
+      }
+    })
+  }
+
+  openEditarModalProvider(providerDto:ProveedoresDto){
+    this._modalService.open(this.myModalConfig,{size:'lg'});
+    this.modalTitle="Modifica"
   }
 
 
--- web-site-control-expenses/src/app/services/proveedores.service.ts
+++ web-site-control-expenses/src/app/services/proveedores.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiResponseDto } from '../interface/api-response-dto';
 import { environment } from '../../environments/environment.development';
+import { ProveedoresDto } from '../interface/proveedores-dto';
 
 @Injectable({
   providedIn: 'root'
@@ -15,6 +16,15 @@ export class ProveedoresService {
   public getAllProviders(token:string):Observable<any>{
     return this._http.get(`${environment.api}/v1/proveedores/get-all`,{headers:{'content-type':'application/json','Authorization':`Bearer ${token}`}});
   }
+
+  public setProvider(providerDto:ProveedoresDto, token:string):Observable<any>{
+    return this._http.post(`${environment.api}/v1/proveedores/save`,providerDto,{headers:
+      {'content-type':'application/json','Authorization':`Bearer ${token}`}
+    })
+  }
+
+
+
 
 
 
