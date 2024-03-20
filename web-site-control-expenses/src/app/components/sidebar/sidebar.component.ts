eaceb8875652b8236ee9d96feb0b79189d2ba09a
Axel200121 <axelmartinez212001@gmail.com>
2024-03-20 09:35:34 -0600

    Agregar condición para diferente tipo de rol

--- web-site-control-expenses/src/app/components/sidebar/sidebar.component.ts
+++ web-site-control-expenses/src/app/components/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,6 +9,16 @@ import { RouterLink } from '@angular/router';
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+
+  constructor(private _authService:AuthService){}
+
+  public perfil!:string
+
+  ngOnInit(): void {
+    this.perfil = this._authService.getPerfil()
+  }
+
+  
 
 }
