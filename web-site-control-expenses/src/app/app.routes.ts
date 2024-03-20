b70a983f04b59ef873e6816aeeceb3f6c58d342c
Axel200121 <axelmartinez212001@gmail.com>
2024-03-19 20:08:37 -0600

    Agregar rutas de los componentes

--- web-site-control-expenses/src/app/app.routes.ts
+++ web-site-control-expenses/src/app/app.routes.ts
@@ -1,9 +1,34 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
+import { ProveedoresComponent } from './pages/proveedores/proveedores.component';
+import { GastosFijosComponent } from './pages/gastos-fijos/gastos-fijos.component';
+import { GastosPorDiaComponent } from './pages/gastos-por-dia/gastos-por-dia.component';
 
 export const routes: Routes = [
     {
         path:"",
         component:HomeComponent
+    },
+    {
+        path:"login",
+        component:LoginComponent
+    },
+    {
+        path:"users",
+        component:UsuariosComponent
+    },
+    {
+        path:"providers",
+        component:ProveedoresComponent
+    },
+    {
+        path:"gastos-fijos",
+        component:GastosFijosComponent
+    },
+    {
+        path:"gastos-por-dia",
+        component:GastosPorDiaComponent
     }
 ];
