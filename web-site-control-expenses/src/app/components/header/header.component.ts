163240db7c0b97ff997fcd25cc2ee75c3eb8ff2a
Axel200121 <axelmartinez212001@gmail.com>
2024-03-19 20:04:12 -0600

    Agrengado la hora en tiempo real en el header

--- web-site-control-expenses/src/app/components/header/header.component.ts
+++ web-site-control-expenses/src/app/components/header/header.component.ts
@@ -1,21 +1,35 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
 import dayjs from 'dayjs';
 import "dayjs/locale/es"
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+  public hora:any;
+
+  ngOnInit(): void {
+    this.getActualHour()
+  }
 
   public getActualDate(){
     dayjs.locale('es')
     let date = new Date()
     return dayjs(date).format("dddd")+" "+dayjs(date).format("DD")+" de "+dayjs(date).format("MMMM")+
     " de "+dayjs(date).format("YYYY")
+  }
+
+  public getActualHour(){
+    this.hora= new Date()
+    setInterval(()=>{
+      this.hora= new Date()
+    },1000)
   }
 
 }
