43eb1dbc52b2645e357b6cba2b8791dffa5825f5
Axel200121 <axelmartinez212001@gmail.com>
2024-03-19 19:44:15 -0600

    Agregnaod componentes sidebar,header y footer

--- web-site-control-expenses/src/app/pages/home/home.component.ts
+++ web-site-control-expenses/src/app/pages/home/home.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from '../../components/footer/footer.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent,HeaderComponent,SidebarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
