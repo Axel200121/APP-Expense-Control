efa320798e53b6d21fbcb79b3eb1af98da14eb01
Axel200121 <axelmartinez212001@gmail.com>
2024-03-20 13:42:24 -0600

    Diseño UI de usuarios

--- web-site-control-expenses/src/app/pages/usuarios/usuarios.component.ts
+++ web-site-control-expenses/src/app/pages/usuarios/usuarios.component.ts
@@ -1,10 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from '../../components/header/header.component';
+import { SidebarComponent } from '../../components/sidebar/sidebar.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-usuarios',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,HeaderComponent,SidebarComponent,FooterComponent],
   templateUrl: './usuarios.component.html',
   styleUrl: './usuarios.component.css'
 })
