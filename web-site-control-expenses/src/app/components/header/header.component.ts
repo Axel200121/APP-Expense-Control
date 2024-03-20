4dee76a3e344844f8dad9f08c0d0da4a66b75786
Axel200121 <axelmartinez212001@gmail.com>
2024-03-19 19:57:28 -0600

    Agregando la fecha actual en el header

--- web-site-control-expenses/src/app/components/header/header.component.ts
+++ web-site-control-expenses/src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import dayjs from 'dayjs';
+import "dayjs/locale/es"
 
 @Component({
   selector: 'app-header',
@@ -8,5 +10,12 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+  public getActualDate(){
+    dayjs.locale('es')
+    let date = new Date()
+    return dayjs(date).format("dddd")+" "+dayjs(date).format("DD")+" de "+dayjs(date).format("MMMM")+
+    " de "+dayjs(date).format("YYYY")
+  }
 
 }
